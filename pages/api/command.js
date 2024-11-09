fc8612bec929707bdfb7b4e2b6517b9c4e934b48
moorsknight11 <hajjejhazem063@gmail.com>
2024-11-09 17:24:24 +0100

    cloudinary use next brand_id

--- pages/api/command.js
+++ pages/api/command.js
@@ -1,14 +1,47 @@
 import transporter from "./transporter";
+import db from "../../lib/db";
 export default async function handler(req, res) {
     try {
         // Send email
+        console.log(req.body)
+
+
+        const sql = `INSERT INTO commande
+        (content)
+        VALUES(?)`;
+
+        // Sample values; adjust them according to your application's needs
+        const values = [
+            JSON.parse(req.body).commande
+            // total_amount (replace with actual amount)
+
+        ];
+
+        function queryDatabase(sql, values) {
+            return new Promise((resolve, reject) => {
+                db.query(sql, values, (error, results) => {
+                    if (error) {
+                        console.error("Database error:", error);
+                        reject(error);
+                    } else {
+                        resolve(results);
+                    }
+                });
+            });
+        }
+
+        const results = await queryDatabase(sql, values)
+
+
+        const insertedId = results.inserteId
+
         console.log(req.body)
         await transporter.sendMail({
             from: JSON.parse(req.body).email, // Sender address
             to: 'hajjejhazem063@gmail.com', // Recipient address
             subject: 'Commande commande!', // Subject line
-            text: "Une commande est là! de la part de "+JSON.parse(req.body).name+ " son email est: "+JSON.parse(req.body).email+" details de la commande est: "+
-            JSON.parse(req.body).commande+" et son telephone est: "+JSON.parse(req.body).phone+ " et son addresse est: "+JSON.parse(req.body).address
+            text: "Une commande est là! de la part de " + JSON.parse(req.body).name + " son email est: " + JSON.parse(req.body).email + " details de la commande est: " +
+                JSON.parse(req.body).commande + " et son telephone est: " + JSON.parse(req.body).phone + " et son addresse est: " + JSON.parse(req.body).address
             // HTML content for email (optional)
             // HTML content for email (optional)
         });
