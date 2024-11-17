ae921ad73d9f16d34ad5f76aebd4dd9cf97b1f9a
moorsknight11 <hajjejhazem063@gmail.com>
2024-11-17 16:01:17 +0100

    cloudinary use next brand

--- pages/api/images/[id].js
+++ pages/api/images/[id].js
@@ -13,16 +13,24 @@ export default async function handler(req, res) {
 
       const productId = imageData[0]?.image_id;
       const [productData] = await db.execute('SELECT product_id FROM images WHERE image_url = ?', [id]);
-      console.log('produtctidtrue',productData)
+      console.log('produtctidtrue', productData)
       const productIdTrue = productData[0]?.product_id;
 
       if (productId) {
         // Delete the image from the images table using its URL
-       
+
         // Update the images_urls column in the product table
+        let updatedImageUrls
         const product = await db.execute('SELECT images_urls FROM product WHERE product_id = ?', [productIdTrue]);
-        const updatedImageUrls = product[0][0]?.images_urls.split(',').filter(imageUrl => imageUrl.trim() !== id.trim()).join(',');
-console.log("product",product)
+        if (product.images_urls.split(',')) {
+
+          updatedImageUrls = product[0][0]?.images_urls.split(',').filter(imageUrl => imageUrl.trim() !== id.trim()).join(',');
+          console.log("product", product)
+        }
+        else {
+          updatedImageUrls = [product[0][0]?.images_urls].push(id).join(',')
+        }
+
         // Update the product's images_urls in the database
         await db.execute('UPDATE product SET images_urls = ? WHERE product_id = ?', [updatedImageUrls, productIdTrue]);
         await db.execute('DELETE FROM images WHERE image_id = ?', [productId]);
